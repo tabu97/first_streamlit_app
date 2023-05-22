b5b5120a37005e7a64af0932349a6a6f97506241
tabu97 <110127047+tabu97@users.noreply.github.com>
2023-05-22 13:08:20 +0530

    Update streamlit_app.py
    adding headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('BreakFast Meau')
-streamlit.text('Omega 3 and blueberry oatmeal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
-streamlit.text('Hard-bolied, free range egg')
+streamlit.text(' 🥣 Omega 3 and blueberry oatmeal')
+streamlit.text(' 🥗 Kale, Spinach and Rocket smoothie')
+streamlit.text(' 🐔 Hard-bolied, free range egg')
+streamlit.text('🥑🍞 Avacado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
