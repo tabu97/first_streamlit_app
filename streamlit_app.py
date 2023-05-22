681f58703de2c568c4821d05da883b3bf31609e1
tabu97 <110127047+tabu97@users.noreply.github.com>
2023-05-22 13:04:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
-streamlit.Header('BreakFast Meau')
+streamlit.header('BreakFast Meau')
 streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Kale, Spinach and Rocket smoothie')
 streamlit.text('Hard-bolied, free range egg')
